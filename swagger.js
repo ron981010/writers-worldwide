4e56dd88e2053bc1257432d2696d8531bd543422
ron981010 <rcacarapi.98@gmail.com>
2023-06-25 23:39:04 -0400

    creating

---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description',
+  },
+  host: 'cse341-l4-luyz.onrender.com',
+  schemes: ['https'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
