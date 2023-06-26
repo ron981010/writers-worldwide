db892fb554eb1cab33d08f5656adf349204ef9dd
ron981010 <rcacarapi.98@gmail.com>
2023-06-26 00:50:26 -0400

    adding controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
-router.use('/contacts', require('./contacts'));
+router.use('/users', require('./users.js'));
 
 module.exports = router;
